139f351caa3bdb82be867744fa19fc65fae2c252
Naga Tejeswara Reddy Surivigari <naga.surivigari@solugenix.com>
2023-11-03 18:30:07 +0530

    commit the base code o the branch

---
+++ src/test/java/com/inverst/service/InvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inverst.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
