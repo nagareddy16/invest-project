daa32cc7d032c4a9bf1fb9d083271c4e23a9e366
Naga <reddynagaa@gmail.com>
2023-11-16 17:22:09 +0530

    feature/exception creation

--- src/main/java/com/inverst/service/Exception/response/ResponseMessage.java
+++ src/main/java/com/inverst/service/Exception/response/ResponseMessage.java
@@ -3,7 +3,9 @@ package com.inverst.service.Exception.response;
 import org.springframework.http.HttpStatus;
 
 public enum ResponseMessage {
-    DATA_NOT_FOUND(HttpStatus.NOT_FOUND,"1049", "No Data Found");
+    DATA_NOT_FOUND(HttpStatus.NOT_FOUND,"1049", "No Data Found"),
+    APPLICATION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "1005", "Application Error"),
+    NO_VALUE_PRESENT(HttpStatus.BAD_REQUEST,"4001" ,"No Value present/Mandatory params missing");
 
 
     private String code;
---
+++ src/main/java/com/inverst/service/utils/Constant.java
@@ -0,0 +1,7 @@
+package com.inverst.service.utils;
+
+public final class Constant {
+
+
+    public static final String PRODUCTION_ENV_PREFIX = "prod";
+}
