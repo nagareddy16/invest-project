c9a0759bdc8a7b00d83218151e4622a4c974e313
Naga <reddynagaa@gmail.com>
2023-12-06 19:16:36 +0530

    log entry logic

--- src/main/java/com/inverst/service/utils/Constant.java
+++ src/main/java/com/inverst/service/utils/Constant.java
@@ -4,4 +4,6 @@ public final class Constant {
 
 
     public static final String PRODUCTION_ENV_PREFIX = "prod";
+    public static final String REQUEST_ID = "requestId";
+    public static final String UNDEFINED = "undefined";
 }
